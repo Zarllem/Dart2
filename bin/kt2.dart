c014c03afd896a34f46f7898cc6b1a361eba05e9
Zarllem <z.zx.zx.zx.zx.z@yandex.ru>
2024-10-12 13:16:30 +0500

    final result

---
+++ bin/kt2.dart
@@ -0,0 +1,7 @@
+import 'provider/api_provider.dart';
+
+void main(List<String> arguments) async {
+  final provider = ApiProvider();
+  final product = await provider.getProductList();
+  print(product);
+}
